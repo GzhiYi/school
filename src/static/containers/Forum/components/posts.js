66469795a664d3fc198bea300b70ddb147b4afe9
GzhiYi <gzy.guy@gmail.com>
2018-01-01 00:49:39 +0800

    added post list style.

--- src/static/containers/Forum/components/posts.js
+++ src/static/containers/Forum/components/posts.js
@@ -1,42 +1,49 @@
 import React, { Component } from 'react';
 import './style.scss';
+import Jpg from '../../../images/github.png';
+import _ from 'lodash';
 
 class Posts extends Component {
     render() {
         let renderPosts = null;
+        let test = [1, 2 , 3, 4, 4, 5, 5];
         renderPosts = 
-            <div className="list-item">
-                <a href="#" className="list-item-avatar">
-                    <img src="../../../images/github.png" alt="头像"/>
-                </a>
-                <div className="list-content">
-                    <a href="#" className="list-title">标题标题</a>
-                </div>
-                <div className="list-footer">
-                    <div className="author">
-                        <a href="#">author</a>
-                    </div>
-                    <div className="read-count">
-                        <i>icon</i>
-                        <p>20</p>
-                    </div>
-                    <div className="comment-count">
-                        <i>icon2</i>
-                        <p>2022</p>
-                    </div>
-                    <div className="last-comment">
-                        <div className="last-commenter">
-                            gzhiyi
+            _.map (test, (item, index) => {
+                return (
+                    <div className="list-item" key = {index}>
+                        <a href="#" className="list-item-avatar">
+                            <img src={Jpg} alt="头像" />
+                        </a>
+                        <div className="list-content">
+                            <a href="#" className="list-title">标题标题</a>
                         </div>
-                        <div className="last-comment-time">
-                            2017-12-31 20:59
+                        <div className="list-footer">
+                            <div className="author">
+                                <a href="#">author</a>
+                            </div>
+                            <div className="read-count">
+                                <i className="fa fa-eye" aria-hidden="true"></i>
+                                <span>20</span>
+                            </div>
+                            <div className="comment-count">
+                                <i className="fa fa-comment" aria-hidden="true"></i>
+                                <span>2022</span>
+                            </div>
+                            <div className="last-comment">
+                                <div className="last-commenter">
+                                    gzhiyi
+                                </div>
+                                <div className="last-comment-time">
+                                    2017-12-31 20:59
+                                </div>
+                            </div>
+                            <div className="post-time">
+                                2017-12-31
+                            </div>
                         </div>
-                    </div> 
-                    <div className="post-time">
-                        2017-12-31
-                    </div> 
-                </div>
-            </div>
+                    </div>
+                )
+            })
         return (
             <div>
                 {renderPosts}
