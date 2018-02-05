453aa08f30c6652aebd87778b4af28f48d292196
GzhiYi <gzy.guy@gmail.com>
2018-02-05 23:24:25 +0800

    added profile form.

--- src/static/containers/Profile/components/basicView.js
+++ src/static/containers/Profile/components/basicView.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import Icon from 'antd/lib/icon';
 import Upload from 'antd/lib/upload';
 import message from 'antd/lib/message';
+import Input from 'antd/lib/input';
+import Button from 'antd/lib/button';
+import Radio from 'antd/lib/radio';
+import DatePicker from 'antd/lib/date-picker';
+import Select from 'antd/lib/select';
+import Spin from 'antd/lib/spin';
+
+const RadioGroup = Radio.Group;
+const Search = Input.Search;
+const Option = Select.Option;
 class BasicView extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			loading: false
+			loading: false,
+			formValues: {
+				firstName: null,
+				lastName: null,
+				phoneNumber: null,
+				gender: null,
+				dateBorn: null,
+				photoUrl: null
+			},
 		}
 	}
 
@@ -42,6 +60,14 @@ class BasicView extends Component {
 		}
 	}
 
+	onInputChange = (e) => {
+		let formValues = this.state.formValues;
+		formValues[e.target.name] = _.trim(e.target.value);
+		this.setState({
+			formValues
+		});
+	}
+
 	render() {
 		const uploadButton = (
 			<div>
@@ -50,7 +76,8 @@ class BasicView extends Component {
 			</div>
 		);
 		const imageUrl = this.state.imageUrl;
-
+		const user = JSON.parse(sessionStorage.getItem('user'));
+		console.log('user', user);
 		return (
 			<div className="basic-view">
 				<div className="avatar-upload">
@@ -65,6 +92,85 @@ class BasicView extends Component {
 					>
 						{imageUrl ? <img src={imageUrl} alt="" /> : uploadButton}
 					</Upload>
+				</div>
+				<div className="basic-form">
+					<div className="form-horizontal">
+						<div className="form-group">
+							<label className="col-md-4 control-label">邮箱</label>
+							<div className="col-md-5">
+								<Input
+									placeholder={user.email}
+									className="profile-input"
+									disabled={true}
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<label className="col-md-4 control-label">用户名</label>
+							<div className="col-md-5">
+								<Input
+									name="firstName"
+									placeholder="FirstName"
+									defaultValue={user.first_name}
+									className="profile-input"
+									onChange={this.onInputChange}
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<label className="col-md-4 control-label">电话号码</label>
+							<div className="col-md-5">
+								<Input
+									name="phoneNumber"
+									placeholder="Phone"
+									// defaultValue={user.phone_number}
+									className="profile-input"
+									onChange={this.onInputChange}
+									type="number"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<label className="col-md-4 control-label">性别</label>
+							<div className="col-md-5">
+								<RadioGroup
+									onChange={this.onInputChange}
+									// defaultValue={user.gender}
+									name="gender"
+								>
+									<Radio value='male'>男</Radio>
+									<Radio value='femile'>女</Radio>
+								</RadioGroup>
+							</div>
+						</div>
+
+						<div className="form-group">
+							<label className="col-md-4 control-label">出生日期</label>
+							<div className="col-md-5">
+								<DatePicker
+									// defaultValue={moment(user.date_born)}
+									onChange={this.onDatePickerChange}
+								/>
+							</div>
+						</div>
+
+						<div className="form-group">
+							<div className="col-md-9 text-right">
+								<Button
+									type="primary"
+									onClick={this.updateProfile}
+								>
+									{
+										this.props.isUpdatingProfile
+											?
+											"Updating..."
+											:
+											"Update Profile"
+									}
+								</Button>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
