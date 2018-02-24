40e6f3b31f188a39a85cec3e5da15ff0335b220b
GzhiYi <gzy.guy@gmail.com>
2018-02-24 22:20:45 +0800

    added filter

--- src/static/containers/Profile/components/postsView.js
+++ src/static/containers/Profile/components/postsView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Table from 'antd/lib/table';
+import Input from 'antd/lib/input';
+const Search = Input.Search;
 
 const columns = [{
 	title: '标题',
@@ -8,30 +10,32 @@ const columns = [{
 }, {
 	title: '发帖时间',
 	dataIndex: 'time',
+	sorter: (a, b) => new Date(a.time) - new Date(b.time),
 }, {
-	title: 'Address',
-	dataIndex: 'address',
+	title: '类别',
+	dataIndex: 'category',
+	sorter: (a, b) => a.category.localeCompare(b.category,  'zh-Hans-CN', {sensitivity: 'accent'})
 }];
 const data = [{
 	key: '1',
-	name: 'John Brown',
-	age: 32,
-	address: 'New York No. 1 Lake Park',
+	title: '今天天气不错',
+	time: '2018-2-2',
+	category: '问答',
 }, {
 	key: '2',
-	name: 'Jim Green',
-	age: 42,
-	address: 'London No. 1 Lake Park',
+	title: '今天天气不错',
+	time: '2018-2-1',
+	category: '求助',
 }, {
 	key: '3',
-	name: 'Joe Black',
-	age: 32,
-	address: 'Sidney No. 1 Lake Park',
+	title: '今天天气不错',
+	time: '2018-2-23',
+	category: '帮忙',
 }, {
 	key: '4',
-	name: 'Disabled User',
-	age: 99,
-	address: 'Sidney No. 1 Lake Park',
+	title: '今天天气不错',
+	time: '2018-2-3',
+	category: '问答',
 }];
 
 const rowSelection = {
@@ -54,6 +58,13 @@ class PostsView extends Component {
 	render() {
 		return (
 			<div>
+				<div className="search">
+					<Search
+						placeholder="标题"
+						onSearch={value => console.log(value)}
+						style={{ width: 200 }}
+					/>
+				</div>
 				<Table rowSelection={rowSelection} columns={columns} dataSource={data} />
 			</div>
 		);
