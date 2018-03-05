3d34e2c03485395badb6ec008bc38f8d73792ff3
GzhiYi <gzy.guy@gmail.com>
2018-03-05 22:30:01 +0800

    fixed admin layout

--- src/static/containers/Admin/index.js
+++ src/static/containers/Admin/index.js
@@ -6,12 +6,18 @@ import Layout from 'antd/lib/layout';
 import './style.scss';
 
 const SubMenu = Menu.SubMenu;
-const { Header, Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 class AdminView extends Component {
     state = {
         openKey: null,
+        collapsed: false,   
         
+    }
+
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
     }
 
     handleClick = (e) => {
@@ -22,31 +28,51 @@ class AdminView extends Component {
     render() {
         return (
             <div className="admin-page">
-                <Layout>
+                <Layout style={{ minHeight: '85.2vh' }}>
                     <Sider
-                        trigger={null}
                         collapsible
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.onCollapse}
                     >
-                        <div className="logo">
-                            {/* <img src={logoImg} /> */}
-                        </div>
-                        <Menu
-                            mode="inline"
-                            theme="dark"
-                            inlineCollapsed={this.state.collapsed}
-                            onClick={this.handleClick}
-                            defaultOpenKeys={['sub1']}
-                            defaultSelectedKeys={['0']}
-                            selectedKeys={["0"]}
-                        >
-                            <SubMenu key="sub1" title={<span><Icon type="edit" /><span>技术测试</span></span>}>
-                                <Menu.Item key="0">第一题</Menu.Item>
-                                <Menu.Item key="1">第二题</Menu.Item>
-                                <Menu.Item key="2">第三题</Menu.Item>
+                        <div className="logo" />
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1">
+                                <Icon type="pie-chart" />
+                                <span>Option 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="desktop" />
+                                <span>Option 2</span>
+                            </Menu.Item>
+                            <SubMenu
+                                key="sub1"
+                                title={<span><Icon type="user" /><span>User</span></span>}
+                            >
+                                <Menu.Item key="3">Tom</Menu.Item>
+                                <Menu.Item key="4">Bill</Menu.Item>
+                                <Menu.Item key="5">Alex</Menu.Item>
                             </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={<span><Icon type="team" /><span>Team</span></span>}
+                            >
+                                <Menu.Item key="6">Team 1</Menu.Item>
+                                <Menu.Item key="8">Team 2</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="9">
+                                <Icon type="file" />
+                                <span>File</span>
+                            </Menu.Item>
                         </Menu>
-
                     </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }} />
+                        <Content style={{ margin: '16px 16px' }}>
+                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                                Bill is a cat.
+                            </div>
+                        </Content>
+                    </Layout>
                 </Layout>
             </div>
         );
