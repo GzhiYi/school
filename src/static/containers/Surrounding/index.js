bc834afbfc6191450db5a29e1838097691944f8e
GzhiYi <gzy.guy@gmail.com>
2018-03-12 22:06:38 +0800

    added tab for surrounding.

--- src/static/containers/Surrounding/index.js
+++ src/static/containers/Surrounding/index.js
@@ -4,11 +4,12 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { push } from 'react-router-redux';
 import PropTypes from 'prop-types';
-
+import Tabs from 'antd/lib/tabs';
+import Icon from 'antd/lib/icon';
 import * as actionCreators from '../../actions/data';
 
 import './style.scss';
-
+const TabPane = Tabs.TabPane;
 class SurroundingView extends React.Component {
     static propTypes = {
         dispatch: PropTypes.func.isRequired,
@@ -29,8 +30,29 @@ class SurroundingView extends React.Component {
 
     render() {
         return (
-            <div>
-                This is surrounding page.
+            <div className="card-container">
+                <Tabs type="card">
+                    <TabPane tab={<span><Icon type="team" />周边听闻</span>} key="1">
+                        <p>Content of Tab Pane 1</p>
+                        <p>Content of Tab Pane 1</p>
+                        <p>Content of Tab Pane 1</p>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="shop" />有什么吃的</span>} key="2">
+                        <p>Content of Tab Pane 2</p>
+                        <p>Content of Tab Pane 2</p>
+                        <p>Content of Tab Pane 2</p>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="coffee" />来喝喝</span>} key="3">
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="car" />去哪</span>} key="4">
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                        <p>Content of Tab Pane 3</p>
+                    </TabPane>
+                </Tabs>
             </div>
         )
     }
